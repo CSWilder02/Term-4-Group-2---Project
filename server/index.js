9b0fb8821242feb855c8daa95eddc85472731909
eddiesosera <21100419@virtualwindow.co.za>
2023-09-22 22:11:51 +0200

    Server Connected to DB

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { json } = require("body-parser");
+
+require("dotenv").config({ path: '.env' })
+
+app.use(cors({ origin: 'https://localhost:3000' })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "dv200_term4_group2_owi_forum" //Collection Name
+    })
+    .then(console.log('Connected to Database'))
+    .catch(err => console.log("No Connection. Error:" + err));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => { console.log(`Listening to Port: ${PORT}`) })
