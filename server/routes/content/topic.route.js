c650e73ac24af5a3ff8faab105498081cbbdd9bf
eddiesosera <21100419@virtualwindow.co.za>
2023-09-24 12:19:10 +0200

    Commnity Model done + Community Route setup

--- server/routes/content/topic.route.js
+++ server/routes/content/topic.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const TopicSchema = require("../../models/content/topic.model");
 
 const router = express();
-const verifyToken = require("../../middleware/auth/verifyToken")
+const verifyToken = require("../../middleware/auth/verifyToken");
 
 // Get All Topics
-router.get("/api/topics", verifyToken, async (req, res) => {
+router.get("/api/getTopics", verifyToken, async (req, res) => {
     try {
         const userId = req.user.userId;
         if (userId || userId !== "") {
@@ -26,13 +26,13 @@ router.get("/api/topic/:id", async (req, res) => {
     await TopicSchema.findById(req.params.id)
         .then(response => res.json(response))
         .catch(error => res.status(500).json(error));
-})
+});
 
 // Delete Topic
 router.delete("/api/deleteTopic/:id", async (req, res) => {
     await TopicSchema.findByIdAndDelete(req.params.id)
         .then(response => res.json(response))
         .catch(error => res.status(500).json(error));
-})
+});
 
 module.exports = router;
