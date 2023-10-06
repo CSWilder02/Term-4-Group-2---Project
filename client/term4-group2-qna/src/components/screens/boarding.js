4bff518ba078df73039376e6b67b83520c1b99a4
DupieM <221326@virtualwindow.co.za>
2023-10-06 22:06:02 +0200

    function to move beteween login & sign up

--- client/term4-group2-qna/src/components/screens/boarding.js
+++ client/term4-group2-qna/src/components/screens/boarding.js
@@ -63,31 +63,75 @@ export const Boarding = ({ user }) => {
             });
     }
 
+    // To switch between Login and Sign Up
+    const loginHeading = document.getElementById("login3");
+    const loginText = document.getElementById("login2");
+    const loginForm = document.getElementById("login");
+    const signupBtn = () => {
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+        loginHeading.style.marginLeft = "-50%";
+    };
+    const loginBtn = () => {
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+        loginHeading.style.marginLeft = "0%";
+    };
+
     return (
         <div className='mainbg'>
 
-            <div className='form'>
-                <div className='heading'>Login</div>
-                <div className='input_fields'>
-                    <input type='text' placeholder='username' onChange={(e) => setLoginData({ ...loginData, username: e.target.value })} />
-                    <input type='password' placeholder='password' onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
-                    <button className='login_button' onClick={submitLogin}>Login</button>
+            <div className="wrapper">
+                <div className='form'>
+                    <div className='title login' id="login3">Login Form</div>
+                    <div className='title signup'>Signup Form</div>
+                </div>
+                <div className="form-container">
+                    <div className="slide-controls">
+                        <input type="radio" name="slide" id="login" checked/>
+                        <input type="radio" name="slide" id="signup"/>
+                        <label for="login" className="slide login" onClick={loginBtn}>Login</label>
+                        <label for="signup" className="slide signup" onClick={signupBtn}>Signup</label>
+                        <div class="slider-tab"></div>
+                    </div>
+                    <div class="form-inner">
+                        <form action="#" className="login" id="login2">
+                            <div className="field">
+                                <input type="text" placeholder="Username" onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}/>
+                            </div>
+                            <div className="field">
+                                <input type="password" placeholder="Password" onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}/>
+                            </div>
+                            <div className="field btn">
+                            <button className='login_button' onClick={submitLogin}>Log In</button>
+                            </div>
+                        </form>
+                        <form action="#" className="signup">
+                            <div className="field">
+                                <input type="text" placeholder="Full Name" onChange={e => setRegisterData({ ...registerData, name: e.target.value })}/>
+                            </div>
+                            <div className="field">
+                                <input type="text" placeholder="Email"  onChange={e => setRegisterData({ ...registerData, email: e.target.value })}/>
+                            </div>
+                            <div className="field">
+                                <input type="text" placeholder="Username" onChange={e => setRegisterData({ ...registerData, username: e.target.value })}/>
+                            </div>
+                            <div className="field_file">
+                                <input type="file" placeholder="Image" className="" onChange={e => setRegisterData({ ...registerData, profileImage: e.target.value })}/>
+                            </div>
+                            <div className="field">
+                                <input type="text" placeholder="Bio" onChange={e => setRegisterData({ ...registerData, bio: e.target.value })}/>
+                            </div>
+                            <div className="field">
+                                <input type="password" placeholder="Password" onChange={e => setRegisterData({ ...registerData, password: e.target.value })}/>
+                            </div>
+                            <div>
+                                <button className='signup_button' onClick={submitRegister}>Register</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-
-            <div className='form'>
-                <div className='heading'>Register</div>
-                <div className='input_fields'>
-                    <input type='text' onChange={e => setRegisterData({ ...registerData, name: e.target.value })} placeholder='Full name' />
-                    <input type='text' onChange={e => setRegisterData({ ...registerData, email: e.target.value })} placeholder='email' />
-                    <input type='text' onChange={e => setRegisterData({ ...registerData, username: e.target.value })} placeholder='username' />
-                    <input type='file' onChange={e => setRegisterData({ ...registerData, profileImage: e.target.value })} placeholder='Image' />
-                    <textarea type='text' onChange={e => setRegisterData({ ...registerData, bio: e.target.value })} placeholder='Bio' />
-                    <input type='password' onChange={e => setRegisterData({ ...registerData, password: e.target.value })} placeholder='password' />
-                    <button className='login_button' onClick={submitRegister}>Register</button>
-                </div>
-            </div>
-
         </div>
     )
 }
