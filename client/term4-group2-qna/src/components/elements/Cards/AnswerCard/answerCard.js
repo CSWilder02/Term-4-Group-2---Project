4697aba754567b5cb3698ad700621e3d59422feb
Bladeyboy54 <221146@virtualwindow.co.za>
2023-10-23 22:41:31 +0200

    answerCard Test

--- client/term4-group2-qna/src/components/elements/Cards/AnswerCard/answerCard.js
+++ client/term4-group2-qna/src/components/elements/Cards/AnswerCard/answerCard.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export const AnswerCard = () => {
+export const AnswerCard = ({ answer, onReplySubmit }) => {
+  const [replyText, setReplyText] = useState("");
+
+  const handleReplySubmit = () => {
+    if (replyText.trim() !== "") {
+      onReplySubmit(answer._id, replyText); // Pass the answer ID and reply text to the parent component
+      setReplyText(""); // Clear the input field
+    }
+  };
+
+  const ReplyCard = ({ reply }) => {
     return (
-        // <div>answerCard</div>
-        <div>
-            
+      <div className="reply-card">
+        <div className="reply-content">
+          <p>{reply.text}</p>
         </div>
-    )
-}
+      </div>
+    );
+  };
+
+  return (
+    // <div>answerCard</div>
+    <div className="answerCardWrap">
+      <div className="answer-card">
+        {answer ? ( // Check if answer is defined
+          <div>
+            <div className="answer-content">
+              <p>{answer.text}</p>
+            </div>
+            <div className="replies">
+              {answer.replies.map((reply) => (
+                <ReplyCard key={reply._id} reply={reply} />
+              ))}
+            </div>
+            <div className="reply-input">
+              <input
+                type="text"
+                placeholder="Add a reply..."
+                value={replyText}
+                onChange={(e) => setReplyText(e.target.value)}
+              />
+              <button onClick={handleReplySubmit}>Reply</button>
+            </div>
+          </div>
+        ) : (
+          <p>No answer available</p>
+        )}
+      </div>
+    </div>
+  );
+};
